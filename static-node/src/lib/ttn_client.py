291944629408befe3b0f4e34ef8db74a23a455ff
Francois Roland <francois.roland@umons.ac.be>
2019-03-26 15:56:28 +0100

    Restore TTN session from non-volatile RAM

--- static-node/src/lib/ttn_client.py
+++ static-node/src/lib/ttn_client.py
@@ -26,21 +26,24 @@ class TtnClient:
         self.join_timeout = join_timeout
 
     def _join(self):
-        self.lora.join(activation=LoRa.OTAA, auth=(self.app_eui, self.app_key), timeout=0, dr=0)
-        chrono = Timer.Chrono()
-        while not self.lora.has_joined() and chrono.read() < self.join_timeout:
-            sleep(1)
-        chrono.stop()
+        self.lora.nvram_restore()
         if not self.lora.has_joined():
-            raise JoinException
+            self.lora.join(activation=LoRa.OTAA, auth=(self.app_eui, self.app_key), timeout=0, dr=0)
+            chrono = Timer.Chrono()
+            while not self.lora.has_joined() and chrono.read() < self.join_timeout:
+                sleep(1)
+            chrono.stop()
+            if not self.lora.has_joined():
+                raise JoinException
 
     def send(self, payload):
         if not self.lora.has_joined():
             self._join()
         s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-        print(s)
         s.setsockopt(socket.SOL_LORA, socket.SO_CONFIRMED, False)
         s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
         s.bind(1)
         s.setblocking(True)
         s.send(payload)
+        s.setblocking(False)
+        self.lora.nvram_save()
--- static-node/test/ttn_client_test.py
+++ static-node/test/ttn_client_test.py
@@ -27,10 +27,12 @@ class TtnClientTest(TestCase):
 
         lora.join.assert_called()
         socket.send.assert_called_with(PAYLOAD)
+        lora.nvram_save.assert_called()
 
+    @patch('ttn_client.sleep')
     @patch('ttn_client.Timer')
     @patch('ttn_client.socket')
-    def test_send_timeout(self, socketClass, Timer, LoRa):
+    def test_send_timeout(self, socketClass, Timer, sleep, LoRa):
         socket = socketClass.socket.return_value
         chrono = Timer.Chrono.return_value
         chrono.read.side_effect = [1, 2, 3, 4]
@@ -43,6 +45,21 @@ class TtnClientTest(TestCase):
 
         lora.join.assert_called()
         socket.send.assert_not_called()
+        lora.nvram_save.assert_not_called()
+
+    @patch('ttn_client.socket')
+    def test_send_with_recovered_session(self, socketClass, LoRa):
+        socket = socketClass.socket.return_value
+        lora = LoRa.return_value
+        lora.has_joined.side_effect = [False, True]
+
+        client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+        client.send(PAYLOAD)
+
+        lora.nvram_restore.assert_called()
+        lora.join.assert_not_called()
+        socket.send.assert_called_with(PAYLOAD)
+        lora.nvram_save.assert_called()
 
 
 if __name__ == '__main__':
