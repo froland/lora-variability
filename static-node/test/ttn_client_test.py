ba6b244c5c37c2c0b687085c3430eb00e529858f
Francois Roland <francois.roland@umons.ac.be>
2019-03-29 13:57:46 +0100

    Migrate tests to pytest with pytest_mock

--- static-node/test/ttn_client_test.py
+++ static-node/test/ttn_client_test.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-from unittest.mock import patch
 from ttn_client import TtnClient, JoinException
 
 APP_EUI = b'00112233445566'
@@ -8,61 +6,65 @@ JOIN_TIMEOUT = 3
 PAYLOAD = [0x00, 0x11, 0x22]
 
 
-@patch('ttn_client.LoRa')
-class TtnClientTest(TestCase):
-    def test_constructor(self, LoRa):
-        client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+def test_constructor(mocker):
+    mocker.patch('ttn_client.LoRa')
+    client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+    assert client is not None
 
-    @patch('ttn_client.sleep')
-    @patch('ttn_client.Timer')
-    @patch('ttn_client.socket')
-    def test_send_payload(self, socketClass, Timer, sleep, LoRa):
-        socket = socketClass.socket.return_value
-        chrono = Timer.Chrono.return_value
-        chrono.read.side_effect = [1, 2]
-        lora = LoRa.return_value
-        lora.has_joined.side_effect = [False, False, True, True]
 
-        client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+def test_send_payload(mocker):
+    LoRa = mocker.patch('ttn_client.LoRa')
+    mocker.patch('ttn_client.sleep')
+    Timer = mocker.patch('ttn_client.Timer')
+    socketClass = mocker.patch('ttn_client.socket')
+    socket = socketClass.socket.return_value
+    chrono = Timer.Chrono.return_value
+    chrono.read.side_effect = [1, 2]
+    lora = LoRa.return_value
+    lora.has_joined.side_effect = [False, False, True, True]
+
+    client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+    client.send(PAYLOAD)
+
+    lora.join.assert_called()
+    chrono.start.assert_called()
+    socket.send.assert_called_with(PAYLOAD)
+    lora.nvram_save.assert_called()
+
+
+def test_send_timeout(mocker):
+    from pytest import raises
+
+    LoRa = mocker.patch('ttn_client.LoRa')
+    mocker.patch('ttn_client.sleep')
+    Timer = mocker.patch('ttn_client.Timer')
+    socketClass = mocker.patch('ttn_client.socket')
+    socket = socketClass.socket.return_value
+    chrono = Timer.Chrono.return_value
+    chrono.read.side_effect = [1, 2, 3, 4]
+    lora = LoRa.return_value
+    lora.has_joined.return_value = False
+
+    client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+    with raises(JoinException):
         client.send(PAYLOAD)
 
-        lora.join.assert_called()
-        chrono.start.assert_called()
-        socket.send.assert_called_with(PAYLOAD)
-        lora.nvram_save.assert_called()
-
-    @patch('ttn_client.sleep')
-    @patch('ttn_client.Timer')
-    @patch('ttn_client.socket')
-    def test_send_timeout(self, socketClass, Timer, sleep, LoRa):
-        socket = socketClass.socket.return_value
-        chrono = Timer.Chrono.return_value
-        chrono.read.side_effect = [1, 2, 3, 4]
-        lora = LoRa.return_value
-        lora.has_joined.return_value = False
-
-        client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
-        with self.assertRaises(JoinException):
-            client.send(PAYLOAD)
-
-        lora.join.assert_called()
-        socket.send.assert_not_called()
-        lora.nvram_save.assert_not_called()
-
-    @patch('ttn_client.socket')
-    def test_send_with_recovered_session(self, socketClass, LoRa):
-        socket = socketClass.socket.return_value
-        lora = LoRa.return_value
-        lora.has_joined.return_value = True
-
-        client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
-        client.send(PAYLOAD)
-
-        lora.nvram_restore.assert_called()
-        lora.join.assert_not_called()
-        socket.send.assert_called_with(PAYLOAD)
-        lora.nvram_save.assert_called()
+    lora.join.assert_called()
+    socket.send.assert_not_called()
+    lora.nvram_save.assert_not_called()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_send_with_recovered_session(mocker):
+    LoRa = mocker.patch('ttn_client.LoRa')
+    socketClass = mocker.patch('ttn_client.socket')
+    socket = socketClass.socket.return_value
+    lora = LoRa.return_value
+    lora.has_joined.return_value = True
+
+    client = TtnClient(APP_EUI, APP_KEY, JOIN_TIMEOUT)
+    client.send(PAYLOAD)
+
+    lora.nvram_restore.assert_called()
+    lora.join.assert_not_called()
+    socket.send.assert_called_with(PAYLOAD)
+    lora.nvram_save.assert_called()
