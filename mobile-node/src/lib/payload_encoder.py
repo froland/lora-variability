9cc016071418322de45aca7cb3c36f9fbaf80cd4
Francois Roland <francois.roland@umons.ac.be>
2019-04-24 14:47:26 +0200

    Encode payload

--- mobile-node/src/lib/payload_encoder.py
+++ mobile-node/src/lib/payload_encoder.py
@@ -2,21 +2,21 @@ def _convert_battery_voltage(input):
     return round((input - 3.0) * 500.0)
 
 
-def _convert_temperature(input):
-    return round((input + 30.0) * 10.0)
+def _convert_latitude(input):
+    return round((input - 49.5) * 30000.0)
 
 
-def _convert_humidity(input):
-    return round(input * 2.0)
+def _convert_longitude(input):
+    return round((input - 2.545) * 20000.0)
 
 
-def _convert_pressure(input):
-    return round((input - 80000.0) / 7.0)
+def _convert_hdop(input):
+    return round(input * 100.0)
 
-
-def encode(battery_voltage: float, temperature: float, humidity: float, barometric_pressure: float):
-    payload = _convert_battery_voltage(battery_voltage) << 30
-    payload += _convert_temperature(temperature) << 20
-    payload += _convert_humidity(humidity) << 12
-    payload += _convert_pressure(barometric_pressure)
+def encode(battery_voltage: float, latitude: float, longitude: float, hdop: float):
+    payload = _convert_battery_voltage(battery_voltage) << 46
+    payload += _convert_latitude(latitude) << 30
+    payload += _convert_longitude(longitude) << 14
+    payload += _convert_hdop(hdop) << 4
+    payload += 0x0F
     return payload
--- mobile-node/test/payload_encoder_test.py
+++ mobile-node/test/payload_encoder_test.py
@@ -2,12 +2,12 @@ import payload_encoder
 
 
 def test_encode():
-    payload = payload_encoder.encode(4.766989, 26.34879, 33.79111, 102286.5)
-    decoded_battery = payload >> 30
+    payload = payload_encoder.encode(4.766989, 50.446150, 3.956785, 1.25)
+    decoded_battery = payload >> 46
     assert decoded_battery == 883
-    decoded_temperature = (payload >> 20) & 0b1111111111
-    assert decoded_temperature == 563
-    decoded_humidity = (payload >> 12) & 0b11111111
-    assert decoded_humidity == 68
-    decoded_pressure = payload & 0b111111111111
-    assert decoded_pressure == 3184
+    decoded_latitude = (payload >> 30) & 0b1111111111111111
+    assert decoded_latitude == 28385
+    decoded_longitude = (payload >> 14) & 0b1111111111111111
+    assert decoded_longitude == 28236
+    decoded_hdop = (payload >> 4) & 0b1111111111
+    assert decoded_hdop == 125
